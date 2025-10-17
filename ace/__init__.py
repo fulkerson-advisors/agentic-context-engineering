2139241c837efd189d630e61b618b27ddab94fcd
cta2106 <32028497+cta2106@users.noreply.github.com>
2025-10-17 22:28:28 +0300

    Release 0.1.2

--- ace/__init__.py
+++ ace/__init__.py
@@ -12,7 +12,7 @@ from ace.storage.sqlite_storage import SQLiteBulletStorage
 from ace.storage.faiss_index import FAISSVectorIndex
 from ace.storage.embedder import OpenAIEmbedder
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 __all__ = [
     "Curator",
